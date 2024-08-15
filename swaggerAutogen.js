fb594baf960c174d7cbd53e7c75ef1cfd837c7e6
busra <kocarslanbusrab@gmail.com>
2024-08-15 07:45:24 +0300

    auth controller and router

--- swaggerAutogen.js
+++ swaggerAutogen.js
@@ -39,7 +39,13 @@ const document = {
 	},
 	security: [{ Token: [] }, { Bearer: [] }],
 	definitions: {
-		"User":require("./src/models/user").schema.obj,
+		"User": require('./src/models/user').schema.obj,
+		"Brand": require('./src/models/brand').schema.obj,
+		"Category": require('./src/models/category').schema.obj,
+		"Firm": require('./src/models/firm').schema.obj,
+		"Product": require('./src/models/product').schema.obj,
+		"Purchase": require('./src/models/purchase').schema.obj,
+		"Sale": require('./src/models/sale').schema.obj,
 		// Models:
 		// "User": require('./src/models/user').schema.obj,
 	}
