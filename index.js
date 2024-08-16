d612d2a119edc09b9490a98747ee43cbac1a7b69
busra <kocarslanbusrab@gmail.com>
2024-08-16 22:25:07 +0300

    syncron

--- index.js
+++ index.js
@@ -95,3 +95,5 @@ app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
 app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
+
+//require('./src/helpers/sync')() // !!! It clear database.
